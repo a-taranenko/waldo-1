92bc2387bef578272cf040de6999c0ef36c08c33
Anton Taranenko <taranenko.anton@gmail.com>
2016-09-08 17:48:18 +0000

    Adds the sorting exercise

---
+++ sorting.js
@@ -0,0 +1,18 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+console.log(students);
+
+function studentSort(a, b) {
+  if (a['age'] > b['age'] && a['name'][0] < b['name'][0]) {
+    return - 1;
+  } else {
+    return 1;
+  }
+}
+
+console.log(students.sort(studentSort));
